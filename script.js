6831556c8d7cf59fca9fc75c9ef2ef3d211fef2a
Matilda Arvidsson <51529201+HIPPIEKICK@users.noreply.github.com>
2020-10-30 15:06:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// These functions are called when a button is clicked.
+
+const toggleAudio = sound => {
+    //Here you write code to toggle whether the audio
+    //is played or paused
+}
+
+const toggleButtonColor = button => {
+  //Here you write code to toggle the button color
+  //between green and orange
+}
